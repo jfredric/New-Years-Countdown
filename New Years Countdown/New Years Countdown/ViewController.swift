3c4b6df6cbca7fac2e3e3cc9315a87c4da82857a
jfredrickson <jfredric@gmail.com>
2020-07-17 16:52:11 -0700

    * Added supoort for dismissing keyboard. * Added support for submitting text from keyboard return. * Applying zipcode from textfield.

--- New Years Countdown/New Years Countdown/ViewController.swift
+++ New Years Countdown/New Years Countdown/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     // MARK: Properties
     
@@ -16,23 +16,59 @@ class ViewController: UIViewController {
     
     // Outlets
     @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var inLabel: UILabel!
     @IBOutlet weak var daysLabel: UILabel!
     @IBOutlet weak var hoursLabel: UILabel!
     @IBOutlet weak var minutesLabel: UILabel!
     @IBOutlet weak var secondsLabel: UILabel!
+    @IBOutlet weak var zipcodeTextField: UITextField!
     
     
     // MARK: View LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
+        // Instantiate our view model
         viewModel = ViewModel()
         
+        // set up keyboard delegation
+        zipcodeTextField.delegate = self
+        
+        // set up gesture to dismiss keyboard when tapping anywhere
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tap)
+        
+        setupKeyboardToolbar()
+        
+        // Start the timer to animate the clock
         Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(self.updateTime), userInfo: nil, repeats: true)
         
+        
     }
+    
+    // MARK: TextField Setup and Delegate Functions
+    
+    func setupKeyboardToolbar() {
+        
+        let toolbar = UIToolbar(frame: CGRect(origin: .zero, size: .init(width: view.frame.size.width, height: 35)))
+        
+        let updateButton = UIBarButtonItem(title: "Update", style: .done, target: self, action: #selector(updateFromKeyboard))
+        // Addes space so we can push button to the right side
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        toolbar.setItems([flexibleSpace, updateButton], animated: false)
+        
+        // Adds toolbar to the textfields accessory view
+        zipcodeTextField.inputAccessoryView = toolbar
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+
+        updateFromKeyboard()
+
+        return true
+    }
+    
     
     // MARK: Hepler Functions
     
@@ -43,22 +79,44 @@ class ViewController: UIViewController {
         minutesLabel.text = String(viewModel.countDown.minutes)
         secondsLabel.text = String(viewModel.countDown.seconds)
     }
+    
+    @objc func updateFromKeyboard() {
+        // Dismiss Keyboard
+        self.view.endEditing(true)
+        
+        updateZipcodeFromTextField()
+    }
+    
+    func updateZipcodeFromTextField() {
+        if let zipcode = zipcodeTextField.text {
+            
+            // TODO: validate zipcode and alert for input
+            
+            TimezoneAPI.shared.getTimezone(zipcode: zipcode) { [weak self] (locationData) in
+                DispatchQueue.main.async {
+                    if let locationData = locationData {
+                        if let inLabel = self?.inLabel {
+                            inLabel.isHidden = false
+                        }
+                        if let locationLabel = self?.locationLabel {
+                            locationLabel.isHidden = false
+                            locationLabel.text = locationData.city + ", " + locationData.state
+                        }
+                        if let viewModel = self?.viewModel {
+                            viewModel.update(timezoneAbbr: locationData.timezone.timezoneAbbr)
+                        }
+                    }
+                }
+            }
+        } else {
+            // TODO: alert invalid input
+        }
+    }
 
     // MARK: Action Functions
     
     @IBAction func submitButtonTapped(_ sender: Any) {
-        TimezoneAPI.shared.getTimezone(zipcode: "90293") { [weak self] (locationData) in
-            DispatchQueue.main.async {
-                if let locationData = locationData {
-                    if let locationLabel = self?.locationLabel {
-                        locationLabel.text = locationData.city + ", " + locationData.state
-                    }
-                    if let viewModel = self?.viewModel {
-                        viewModel.update(timezoneAbbr: locationData.timezone.timezoneAbbr)
-                    }
-                }
-            }
-        }
+        updateZipcodeFromTextField()
     }
     
 
